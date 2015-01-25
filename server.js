974e3a5897fe0768779bb828b08c9d081eeeb892
Vaclav Klecanda <vencax77@gmail.com>
2015-01-25 17:53:03 +0100

    cleanup

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 
 require('coffee-script/register');
-var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 1080;
 
 var app = require('express')();
-app.use(bodyParser.json());
 
 require('./index').initApp(app)
 
